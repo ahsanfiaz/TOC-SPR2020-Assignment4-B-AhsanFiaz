1b15d01a7c748ee2be73975a49ba0dae880199bf
ahsanfiaz <64947095+ahsanfiaz@users.noreply.github.com>
2020-07-14 16:22:39 +0500

    upload

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void hummingBird()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void penguin()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+}
